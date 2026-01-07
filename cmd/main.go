9ed68e8d2d8f7b1f20855bcf6854a1c35326007d
behrang-chaichi <behrangchaichi.coding@gmail.com>
2026-01-07 13:00:07 +0330

    I am working with jsons and I am making some advances after this I start my cli task managment app

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type user struct {
+	UserId      int      `json: "user_id"`
+	Name        string   `json: "name"`
+	Age         int      `json: "age"`
+	Password    string   `json: "password"`
+	Permissions []string `json: "roles"`
+}
 
 func main() {
-	fmt.Println("This is a task manager app")
+
+	// b, err := os.ReadFile("jsonOutput.json")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	panic(err)
+	// }
+
+	f, err := os.Open("jsonOutput.json")
+	if err != nil {
+		fmt.Println(err)
+		panic(err)
+	}
+	defer f.Close()
+
+	var person user
+
+	decoder := json.NewDecoder(f)
+	err = decoder.Decode(&person)
+	fmt.Println(person)
+
 }
